81a174477ffb2921c004ccca7dcfd5ba5bd7b94f
SnowFox76 <jp.steyn.work@gmail.com>
2022-06-23 13:12:54 +0200

    Implemented Hashing & Starting testing of  format printing
    The Hashing of the key was implemented to the main encryption and decryption files.
    I have started working on the format the Unicode characters are printed in the output.

---
+++ minor_testing.py
@@ -0,0 +1,18 @@
+import numpy as np
+import pandas as pd
+
+
+#size = 167
+size = 168
+
+number_list = []
+for i in range(1, size) :
+    if size % i == 0 :
+        print(i)
+        number_list.append(i)
+    else :
+        pass
+print ('Completed')
+
+table_column_size = [5,8,10,12,15]
+
